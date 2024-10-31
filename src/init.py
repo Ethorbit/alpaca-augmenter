f47ff88065fee48274e35d602ed4d9f0fbf64209
Ethorbit <Ethorbit@protonmail.com>
2024-10-31 01:18:06 -0700

    Add multithreading
    Each line in a file is processed synchronously, but max_passes will be multithreaded. This basically means if max_passes is 1, there will be absolutely no multithreading, but if you're enlarging your dataset you will likely have many passes, thus you will certainly benefit from the multithreading support.

--- src/init.py
+++ src/init.py
@@ -1,42 +1,112 @@
-import argparse
 import os
-import json
+import argparse
+from concurrent.futures import (
+    ThreadPoolExecutor
+)
 import nlpaug.augmenter.word as naw
+# import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-f",
-    "--file",
-    type=str,
-    help="Jsonl file to augment"
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    type=str,
-    help="Directory to store augmented file"
-)
 
-parser.add_argument(
-    "--maxpasses",
-    type=int,
-    default=1,
-    help="How many times to rephrase every line in the file."
-)
+class AugmentOptions():
+    synonym_aug: naw.SynonymAug = None
+    max_threads: int = 1
+    max_passes: int = 1
 
-args = parser.parse_args()
-file = args.file
-output = args.output
 
-if not os.path.exists(file):
-    raise Exception("--file specified is an invalid file.")
-if not os.path.exists(output) or not os.path.isdir(output):
-    raise Exception("--output specified is not a directory.")
+def augment_line(
+    options: AugmentOptions,
+    line: str,
+    destination: str
+):
+    print(f"Processing {line}", flush=True)
+    print(options.synonym_aug.augment(line))
 
-print("Ready to start reading jsonl.")
 
-# 1. For each line in the jsonl file, create a thread that will rephrase it, append to output file
-# 2. Run code in a loop for max_passes value, default 1
+def augment_file(
+    options: AugmentOptions,
+    file: str,
+    destination: str
+):
+    # Only use the amount of threads we will actually need
+    max_threads = min(options.max_threads, options.max_passes)
 
-augmenter = naw.SynonymAug(aug_p=0.1)
-#augmented_data = augmenter.augment(example)
+    print(f"Using {max_threads} threads.")
+
+    with open(file, "r") as file:
+        for line in file:
+            with ThreadPoolExecutor(
+                max_workers=max_threads
+            ) as executor:
+                for future in [
+                    executor.submit(
+                        augment_line,
+                        options,
+                        line,
+                        destination
+                    ) for _ in range(max_threads)
+                ]:
+                    try:
+                        future.result()
+                    except Exception as e:
+                        print(f'''
+                            Exception occurred when augmenting line: {line}
+                            {e}
+                        ''')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        help="Jsonl file to augment"
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Directory to store augmented file"
+    )
+
+    parser.add_argument(
+        "--max_passes",
+        type=int,
+        default=1,
+        help="How many times to rephrase every line in the file."
+    )
+
+    cpu_count = os.cpu_count()
+    parser.add_argument(
+        "--max_threads",
+        type=int,
+        default=cpu_count,
+        help="How many CPU threads to make use of."
+    )
+
+    args = parser.parse_args()
+    file = args.file
+    file_name = os.path.basename(file)
+    output_dir = args.output
+
+    if not os.path.exists(file):
+        raise Exception("--file specified is an invalid file.")
+    if not os.path.exists(output_dir) or not os.path.isdir(output_dir):
+        raise Exception("--output specified is not a directory.")
+
+    output_file = os.path.join(output_dir, file_name)
+
+    # Don't allow user input to exceed processor count
+    max_threads = min(args.max_threads, max(1, cpu_count))
+
+    aug_options = AugmentOptions()
+    aug_options.synonym_aug = naw.SynonymAug(aug_p=0.1)
+    aug_options.max_threads = max_threads
+    aug_options.max_passes = args.max_passes
+
+    augment_file(
+        aug_options,
+        file,
+        output_file
+    )
